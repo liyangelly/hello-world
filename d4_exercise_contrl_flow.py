37ec333537512b07dd4a2cfade30ecc29b5cea8c
Elly Lee <18616501006@163.com>
2019-06-12 23:41:26 +0800

    d4.2

--- d4_exercise_contrl_flow.py
+++ d4_exercise_contrl_flow.py
@@ -1,16 +1,26 @@
 #ֵ使用for/while循环打印九九乘法表，while循环需要剔除偶数的行
 a = 0 #创建整数型变量
 b = 0 #创建整数型变量
-x = input('请选择控制结构[for/whilewhile]' '\n') #选择控制结构
+x = input('请选择控制结构[for/while]' '\n') #选择控制结构
 if x == 'for': #选择控制结构为for循环时使用
-    for a in range(1,10): #外层for循环1-9，控制行数
-        for b in range(1,a):      #内层for循环1-a-1,控制列数
+    for a in range(1,9): #外层for循环1-9，控制行数
+        for b in range(1,a+1):      #内层for循环1-a-1,控制列数
             print(a,'*',b,'=',a*b,end='\t') #输出非最后一列的值，中间用制表符隔开
-        print(a,'*',b+1,'=',a*(b+1)) #输出最后一列的值，用换行符换行
+            if b == a:
+                print(end='\n')
 elif x == 'while': #选择控制结构为while时使用
-    for a in range(1,10,2):#从1开始，步数为2，到10之前结束
-        for b in range(1,a):
+    a = 1
+b = 1
+while(a<10):
+    while(a%2 != 0):
+        while(b <= a):
             print(a,'*',b,'=',a*b,end='\t')
-        print(a,'*',b+1,'=',a*(b+1))
+            b=b+1
+        else:
+            print(end='\n')
+        break
+    a += 1
+    b=1
 
+            
   
